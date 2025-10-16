c66177f140af60052b541e2c1d5b3fa0711c22a7
Vithor Roder <vithvith22@gmail.com>
2025-10-16 11:50:58 -0300

    Commit 0003. Fixed Organization Code.

--- src/main/java/br/comphoenix/api/quote/types/QuoteTypeController.java
+++ src/main/java/br/comphoenix/api/quote/types/QuoteTypeController.java
@@ -4,7 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/quote/type")
+@RequestMapping("/api/quote/type/")
 public class QuoteTypeController {
 
     private final QuoteTypeRepository repo;
