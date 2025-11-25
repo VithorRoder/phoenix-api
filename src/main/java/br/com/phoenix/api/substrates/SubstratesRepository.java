2ad405db649dcd2f47690fb746faf840ed2895f9
Vithor Roder <vithvith22@gmail.com>
2025-11-25 15:33:25 -0300

    feat(api): add SubstratesRepository interface Provides a JpaRepository for the Substrates entity to handle basic CRUD operations.

---
+++ src/main/java/br/com/phoenix/api/substrates/SubstratesRepository.java
@@ -0,0 +1,5 @@
+package br.com.phoenix.api.substrates;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SubstratesRepository extends JpaRepository<Substrates, Long> {}
