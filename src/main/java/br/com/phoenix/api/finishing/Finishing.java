b8163a9e78b421d6aad5a0628ceebcad468f2f39
Vithor Roder <vithvith22@gmail.com>
2025-12-11 15:23:00 -0300

    feat(api): add Finishing JPA entity

---
+++ src/main/java/br/com/phoenix/api/finishing/Finishing.java
@@ -0,0 +1,33 @@
+package br.com.phoenix.api.finishing;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "finishing")
+public class Finishing {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
