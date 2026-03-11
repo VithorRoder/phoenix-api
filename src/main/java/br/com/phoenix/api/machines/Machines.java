6952b2c4ed5cc4bcb7a79d485b7c216b2022fa62
Vithor Roder <vithvith22@gmail.com>
2026-03-11 10:18:12 -0300

    feat(api): add Machines JPA entity

---
+++ src/main/java/br/com/phoenix/api/machines/Machines.java
@@ -0,0 +1,55 @@
+package br.com.phoenix.api.machines;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+
+@Entity
+@Table(name = "machines")
+public class Machines {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String code;
+
+    @NotBlank
+    private String type;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+}
